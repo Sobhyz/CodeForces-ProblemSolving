12e17cdc6bb7b3201491f04edb8176cc47594ffe
Sobhyz <asobhy540@gmail.com>
2023-07-29 16:22:17 +0300

    .

--- CountTriplets.cpp
+++ CountTriplets.cpp
@@ -11,13 +11,16 @@ int main(){
     for(int i=0;i<n;i++){cin>>x;freqa[x]++;}
     for(int i=0;i<n;i++){cin>>x;freqb[x]++;}
     for(int i=0;i<n;i++){cin>>x;freqc[x]++;}
-    for(int i=1;i*i<m;i++){
+    cout<<endl;
+    for(int i=1;i*i<=m;i++){
         if(m%i==0){
             int num=m/i;
             for(int j=i;j*j<=num;j++){
                 if(num%j==0){
                     int tmp=num/j;
-                    ans+=((freqa[i]*freqb[j]*freqc[tmp]) + (freqc[i]*freqa[j]*freqb[tmp]) + (freqb[i]*freqc[j]*freqa[tmp]));
+                    cout<<i<<' '<<j<<' '<<tmp<<endl;
+                    ans+=((freqa[i]*freqb[j]*freqc[tmp]) + (freqa[i]*freqc[j]*freqb[tmp]) + (freqb[i]*freqc[j]*freqa[tmp]) 
+                    + (freqb[i]*freqa[j]*freqc[tmp]) + (freqc[i]*freqa[j]*freqb[tmp]) + (freqc[i]*freqb[j]*freqa[tmp]));
                 }
             }
         }
